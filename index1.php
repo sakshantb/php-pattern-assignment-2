16c56a9e4ea6313b6698711f3119492554cfe04f
sakshant <sakshant39@gmail.com>
2022-03-20 16:16:44 +0530

    first commit

---
+++ index1.php
@@ -0,0 +1,14 @@
+<?php
+
+for($i=1;$i<=6;$i++){
+    for($k=6;$k>$i;$k--){
+        echo "&nbsp;&nbsp;";
+    }
+    for($x=0;$x<$i;$x++){
+        echo "*","&nbsp;&nbsp;";
+
+    }
+    
+    echo "<br><br>";
+}
+?>
---
+++ index2.php
@@ -0,0 +1,30 @@
+<?php
+     for($i=1;$i<=5;$i++){
+         for($j=1;$j<=5;$j++){
+             if($j==1 || $j==5){
+                 echo "*";
+
+             }
+             else{
+                 if($i==1 || $i==5){
+                     echo "*";
+                 }
+                 else{
+                     echo "&nbsp;&nbsp;";
+                }
+        }
+           
+
+    }
+         echo "<br>";
+}
+    
+
+     
+   
+     
+?>
+   
+
+
+

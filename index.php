a1f4bdd4eece8b5a47729bb2de3e1f3c8df96230
sakshantb <73871863+sakshantb@users.noreply.github.com>
2022-03-25 15:43:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,14 @@
 <?php
-     for($i=1;$i<=6;$i++){
-        for($x=0;$x<$i;$x++){
-            echo "*";
-
-        }
-        
-        echo "<br><br>";
+for ($i = 1; $i <= 6; $i++) {
+    for ($x = 0; $x < $i; $x++) {
+        echo "*";
     }
-    for($i=5;$i>=1;$i--){
-        for($x=0;$x<$i;$x++){
-            echo "*";
-
-        }
-        echo "<br><br>";
-       
+    echo "<br><br>";
+}
+for ($i = 5; $i >= 1; $i--) {
+    for ($x = 0; $x < $i; $x++) {
+        echo "*";
     }
-    
+    echo "<br><br>";
+}
 ?>
-   
-
-
-
